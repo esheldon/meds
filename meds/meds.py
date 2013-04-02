21d4597eaabf6adaa93d235f144ccafb66b1f2d9
Erin Sheldon <erin.sheldon@gmail.com>
2013-04-02 12:30:49 -0700

    more docs

--- meds/meds.py
+++ meds/meds.py
@@ -3,7 +3,20 @@ Defines the MEDS class to work with MEDS (Multi Epoch Data Structures)
 
 See docs for the MEDS class for more info
 
-Author: Erin Sheldon, BNL
+    Copyright (C) 2013, Erin Sheldon, BNL
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 class MEDS(object):
--- src/meds.h
+++ src/meds.h
@@ -127,6 +127,21 @@
    const struct meds_obj *obj=meds_get_obj(meds, iobj);
    meds_obj_print(obj, stdout);
 
+    Copyright (C) 2013, Erin Sheldon, BNL
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 #ifndef _MEDS_INCLUDE_HEADER_GUARD
 #define _MEDS_INCLUDE_HEADER_GUARD
