ae74e79cc45e041d03f8b58cb1b98b170197d484
Erin Sheldon <erin.sheldon@gmail.com>
2013-09-12 11:50:56 -0400

    updated meds extraction tool

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ import os
 import glob
 from distutils.core import setup
 
+scripts=['meds-extract-range']
+
+scripts=[os.path.join('./scripts', s) for s in scripts]
+
 setup(name="meds", 
       version="0.1.0",
       description="Python and C libraries for reading MEDS files",
       license = "GPL",
       author="Erin Scott Sheldon",
       author_email="erin.sheldon@gmail.com",
-      packages=['meds'])
+      packages=['meds'],
+      scripts=scripts)
 
