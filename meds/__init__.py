0ea5d76368cb88f7e7577526579cdd15fb127211
Matthew R Becker <becker.mr@gmail.com>
2015-01-27 02:19:43 -0800

    adding extractor for random inds; has been tested

--- meds/__init__.py
+++ meds/__init__.py
@@ -2,3 +2,4 @@ from . import meds
 from .meds import MEDS
 from .meds import split_mosaic, reject_outliers
 from .extractor import MEDSExtractor, extract_range, extract_catalog
+from .number_extractor import MEDSNumberExtractor, extract_numbers
