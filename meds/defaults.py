271f6b92265eb657af59723a03cfb6732ed7d17a
Matthew R Becker <becker.mr@gmail.com>
2016-03-06 09:31:48 -0800

    added options to catch all objects for the coadd

--- meds/defaults.py
+++ meds/defaults.py
@@ -3,6 +3,16 @@ default_config = {
     # in arcsec
     'bounds_buffer_uv': 16.0,
 
+    # buffer for "coadd" (first entry) image in the meds in pixels
+    # objects within the coadd image bounds plus this buffer are 
+    # "in" the coadd image
+    'coadd_bounds_buffer_rowcol': 1e-3,
+    
+    # objects within 'coadd_bounds_buffer_rowcol' buffer around 
+    # the bounds of the coadd are forced to be at the edge of 
+    # the chip if 'force_into_coadd_bounds' is True    
+    'force_into_coadd_bounds': True,
+
     # allowed values in the bitmask image
     'bitmask_allowed': 0,
 
