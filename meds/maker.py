9cea2b7285d0649ee9a33d19d535a1011433f30a
Erin Sheldon <erin.sheldon@gmail.com>
2017-03-02 12:13:38 -0800

    use small start nmax

--- meds/maker.py
+++ meds/maker.py
@@ -815,9 +815,11 @@ class MEDSMaker(dict):
         make a full object structure, adding in any extra fields from the
         input structure.  Copy over the common fields
         """
-        nmax = self.image_info.size
-        if nmax < 2:
-            nmax = 2
+        #nmax = self.image_info.size
+        #if nmax < 2:
+        #    nmax = 2
+        # we will fix this later
+        nmax=2
 
         self._set_extra_fields(obj_data, nmax)
 
