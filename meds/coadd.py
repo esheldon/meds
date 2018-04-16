f3c652cbadd231340ccfac5929bb2fd2eb174bda
Erin Sheldon <erin.sheldon@gmail.com>
2018-04-16 13:22:02 -0400

    psf stuff is now to be in subclass

--- meds/coadd.py
+++ meds/coadd.py
@@ -674,49 +674,7 @@ class MEDSCoadder(dict):
         for psfex we need to add 0.5 to get an offset
         that is the same as used for the object
         """
-        import ngmix
-        assert self['psf']['type']=='psfex',"only psfex for now"
-
-        pmeta={}
-
-        ii=self.m.get_image_info()
-        path=ii['image_path'][file_id]
-        key = extract_key(path)
-
-        rowget=row+0.5
-        colget=col+0.5
-        p = self.psfmap[key]
-        pim = p.get_rec(rowget, colget)
-        pcen = p.get_center(rowget, colget)
-        ccen=(np.array(pim.shape)-1.0)/2.0
-
-        # for psfex we assume the jacobian is the same, not
-        # quite right
-        pjac = obs.jacobian.copy()
-        pjac.set_cen(row=pcen[0], col=pcen[1])
-
-        pmeta['offset_pixels']=dict(
-            row_offset=pjac.row0-ccen[0],
-            col_offset=pjac.col0-ccen[1],
-        )
-
-        """
-        print("psf dims:",pim.shape)
-        print("ccen:",ccen)
-        print("pcen:",pcen)
-        print("image offset pixels:",meta['offset_pixels'])
-        print("psf   offset pixels:",pmeta['offset_pixels'])
-        """
-
-        psf_weight=np.zeros(pim.shape) + 1.0/0.001**2
-
-        psf_obs = ngmix.Observation(
-            pim,
-            weight=psf_weight,
-            jacobian=pjac,
-            meta=pmeta,
-        )
-        return psf_obs
+        raise NotImplementedError("implement psf specifics in a sub class")
 
     def _interp_bad_pixels(self, obs):
         """
@@ -872,22 +830,4 @@ class MEDSCoadder(dict):
         print("    writing:",fname)
         tab.write_img(800,800,fname)
 
-def extract_key(path):
-    """
-    expecting D00239652_i_c14_r2362p01_immasked_nullwt.fits
-    """
-    path=makestr(path)
-    bname=os.path.basename(path).strip()
-    bs=bname.split('_')
-    expname=bs[0][1:]
-    ccdstr=bs[2][1:]
 
-    key='%s-%s' % (expname, ccdstr)
-    return key
-
-def makestr(data):
-    try:
-        s=str(data,'utf8')
-    except:
-        s=str(data)
-    return s
